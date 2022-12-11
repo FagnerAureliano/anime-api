d86185696a8d8fd5f9e755ef83c8a337b2b4eb58
fagneraureliano <fagner2aureliano@gmail.com>
2022-12-11 11:36:03 -0300

    impl rest template POST

--- src/main/java/com/example/restapidevdojo/client/SpringClient.java
+++ src/main/java/com/example/restapidevdojo/client/SpringClient.java
@@ -3,8 +3,7 @@ package com.example.restapidevdojo.client;
 import com.example.restapidevdojo.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -23,10 +22,28 @@ public class SpringClient {
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
         log.info(Arrays.toString(animes));
 
-        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+        ResponseEntity<List<Anime>> exchange =  new RestTemplate().exchange("http://localhost:8080/animes/all",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<Anime>>() {});
-        log.info(exchange);
+        log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+//        log.info("saved anime {}", kingdomSaved);
+
+        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChamploo, createJsonHeader()),
+                Anime.class
+                );
+        log.info("saved anime {}", samuraiChamplooSaved);
+    }
+
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders httpHeaders  = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
