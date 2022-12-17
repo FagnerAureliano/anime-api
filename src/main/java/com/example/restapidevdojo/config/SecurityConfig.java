fa4937d7d7cdd4a24e62e606bc7971604d7328a5
fagneraureliano <fagner2aureliano@gmail.com>
2022-12-17 17:19:18 -0300

    impl spring security and two profiles

---
+++ src/main/java/com/example/restapidevdojo/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.restapidevdojo.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("password encoded : {}", passwordEncoder.encode("test"));
+
+        auth.inMemoryAuthentication()
+                .withUser("fagner")
+                .password(passwordEncoder.encode("test"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("test"))
+                .roles("USER");
+    }
+}
